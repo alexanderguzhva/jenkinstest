c6874b1b32a6ec5286c4b068d3dd1ae7c0870323
Alexander Guzhva <alexanderguzhva@gmail.com>
2015-03-08 19:27:35 -0400

    added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nop on 3/8/15.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Class1 class1 = new Class1();
+        System.out.println(class1.getSomeString());
+    }
+}
